1e253e06f2414376557e675af28769db02c51837
NasBench <NasreddineBencherchali@users.noreply.github.com>
2018-11-04 22:15:59 +0100

    Version 0.3
    - Added more tags that contain known XSS vulnerabilities.
    - Changed the way of how the checks are done to a more optimized way.

--- XSS-Finder.py
+++ XSS-Finder.py
@@ -37,19 +37,49 @@ regex_list = []
 
 # JSF / PrimeFaces
 
-# If the dialog contains a "selectItems" tag, an XSS might happen in when it pop's up
-regex_list.append(re.compile(r'<p:dialog.*</p:dialog>', re.MULTILINE | re.DOTALL | re.IGNORECASE))
+# The "title" attribute doesn't escape XSS in PrimeFaces prior to 6.3 
+regex_list.append(re.compile(r'<p:tab.*title.*>'))
+regex_list.append(re.compile(r"<p:commandButton.*title.*/>"))
 
-# If mojarra is < 2.2.6 this is maybe vulnerable to XSS
-regex_list.append(re.compile(r"<f:selectItems.*itemLabel.*\/>"))
+# The "headerText" attribute doesn't escape XSS in PrimeFaces prior to 6.3
+regex_list.append(re.compile(r'<p:carousel.*headerText.*/>'))
+
+# The "footerText" attribute doesn't escape XSS in PrimeFaces prior to 6.3
+regex_list.append(re.compile(r'<p:carousel.*footerText.*/>'))
+
+# The "emptyMessage" attribute doesn't escape XSS in PrimeFaces prior to 6.3
+regex_list.append(re.compile(r"<p:dataGrid.*emptyMessage.*/>"))
+regex_list.append(re.compile(r"<p:dataList.*emptyMessage.*/>"))
+regex_list.append(re.compile(r"<p:treeTable.*emptyMessage.*/>")) 
+
+# The "addLabel" attribute doesn't escape XSS in PrimeFaces prior to 6.3
+regex_list.append(re.compile(r"<p:pickList.*addLabel.*/>"))
+
+# The "labelTemplate" attribute doesn't escape XSS in PrimeFaces prior to 6.3
+regex_list.append(re.compile(r"<p:progressBar.*labelTemplate.*/>"))
+
+# The "backLabel" attribute doesn't escape XSS in PrimeFaces prior to 6.3
+regex_list.append(re.compile(r"<p:slideMenu.*backLabel.*/>"))
+
+# This is vulnerable to XSS, if it contains a "<p:column" tag, and PrimeFaces is > 6.3
+regex_list.append(re.compile(r'<p:selectOneMenu[^>]*>(.+?)</p:selectOneMenu\s*>', re.IGNORECASE|re.MULTILINE|re.DOTALL))
+regex_list.append(re.compile(r'<p:treeTable[^>]*>(.+?)</p:treeTable\s*>', re.IGNORECASE|re.MULTILINE|re.DOTALL))
+
+# This is vulnerable to XSS, if it contains a "<f:selectItems" tag, and PrimeFaces is > 6.0.2
+regex_list.append(re.compile(r'<p:selectManyMenu[^>]*>(.+?)</p:selectManyMenu\s*>', re.IGNORECASE|re.MULTILINE|re.DOTALL))
+
+# If mojarra is < 2.2.6 this is vulnerable to XSS (Uncomment this if you're sure of the version of mojarra)
+# regex_list.append(re.compile(r"<f:selectItems.*itemLabel.*\/>"))
+
+# Vulnerable to XSS in PrimeFaces < 6.0.30 ; 6.1.16 ; 6.2.1
+regex_list.append(re.compile(r"<p:inputTextarea.*completeMethod.*/>"))
+regex_list.append(re.compile(r"<p:inputTextarea.*counterTemplate.*/>"))
 
 # If we have controle over the data this is vulnerable to XSS
-regex_list.append(re.compile(r"<h:outputLink.*\/>"))
+regex_list.append(re.compile(r"<h:outputLink.*/>"))
 
 # If "escape" is present and it's equal to "False" this is vulnerable to XSS
-regex_list.append(re.compile(r"<h:outputText.*escape.*\/>"))
-regex_list.append(re.compile(r"<p:outputLabel.*escape.*\/>"))
-regex_list.append(re.compile(r".*escape=.*\/>"))
+regex_list.append(re.compile(r".*escape=.*/>"))
 
 if len(regex_list) == 0:
     print("[*] You're Regex List is empty [*]")
@@ -61,58 +91,175 @@ with open("TagList.txt", "w") as TagListFile:
         with open(each_page, "r") as page:
             # Grab the content of the file as it is
             page_content = page.read()
-
             # Start looping through our regular expressions list
             for regex in regex_list:
-                str_regex = regex.__repr__()
+                str_regex = regex.__repr__()[10:]
                 try:
                     # Grab the result of the regex search, then we have two cases
                     list_of_tags = regex.findall(page_content)
-
+                    
                     if list_of_tags != [] :
-                        # First case : it's a "<p:dialog> </p:dialog>" tag
-                        if "p:dialog" in str_regex:
-
-                            # We search if contains an "<f:selectItems />" tag
-                            itemLabel_Regex = re.compile(r"<f:selectItems.*itemLabel.*\/>")
-                            for each_dialog_tag in list_of_tags:
-                                tmp_res = itemLabel_Regex.findall(each_dialog_tag)
-
-                                # Then for each element we check if it contains dynamic data (That can be controlled by the user)
-                                for each_selectItems_tag in tmp_res:
-                                    if "bundle" in each_selectItems_tag and "#{" not in each_selectItems_tag:
-                                        # If it doesn't, we remove the element
-                                        tmp_res.remove(each_selectItems_tag)
-                                
-                                # If the list is empty, that means that the dialog tag doesn't contain any data that we can modify 
-                                if tmp_res == []:
-                                    list_of_tags.remove(each_dialog_tag)
-                            
-                            # Else we write the result with the file name it came from 
-                            if list_of_tags != []:
-                                TagListFile.write("REGEX USED : " + str_regex + "\n")
-                                TagListFile.write("You can find this in : " + each_page + "\n\n")
-                                for each_dialog_tag in list_of_tags:
-                                    for each_line in each_dialog_tag:
-                                        TagListFile.write(each_line)
-                                TagListFile.write("\n\n")
                         
-                        # Second Case : Everything else
+                        # First Case : it's a "<p:selectMantMenu>" tag
+                        if "p:selectManyMenu" in str_regex:
+                            filtered_list = []
+                            p_selectItems_regex = re.compile(r"<f:selectItems.*itemLabel.*\/>")
+                            for elements in list_of_tags:
+                                org_elements = elements
+                                xss_itemLabel = elements.find('itemLabel')
+                                if xss_itemLabel == -1:
+                                    break
+                                else:
+                                    # We search for the attribute position then strip the start till the position 
+                                    # Then search fro the end quote of the attribute and the we concatenate both ends
+                                    # Example : <p:test attr="this the value" other="other value" />
+                                    # Results : "this the value"
+                                    elements = elements[xss_itemLabel + len("itemLabel") + 2:][:elements[xss_itemLabel
+                                                + len("itemLabel") + 2:].find('"')]
+                                
+                                if "bundle" not in elements and "#{" in elements:
+                                    elements = org_elements
+                                    filtered_list.append(elements.strip("\n"))
+                                
+                            if filtered_list != []:
+                                TagListFile.write("REGEX : " + str_regex + " \n\n")
+                                TagListFile.write("You can find this in : " + each_page[len(pages_path):] + "\n\n")
+                                for elements in filtered_list:
+                                    TagListFile.write(elements + "\n")
+                                TagListFile.write("\n\n")
+                                TagListFile.write("-" * 20)
+                                TagListFile.write("\n")
+                        
+                        # Second Case : "<p:selectOneMenu" tag
+                        elif "p:selectOneMenu" in str_regex or "p:treeTable" in str_regex:
+                            filtered_list = []
+                            p_column_regex = re.compile(r"<p:column.*headerText.*/>")
+                            for elements in list_of_tags:
+                                org_elements = elements
+                                xss_headerText = elements.find('headerText')
+                                if xss_headerText == -1:
+                                    break
+                                else:
+                                    elements = elements[xss_headerText + len("headerText") + 2:][:elements[xss_headerText
+                                                + len("headerText") + 2:].find('"')]
+                                
+                                if "bundle" not in elements and "#{" in elements:
+                                    elements = org_elements
+                                    filtered_list.append(elements.strip("\n"))
+                                
+                            if filtered_list != []:
+                                TagListFile.write("REGEX : " + str_regex + " \n\n")
+                                TagListFile.write("You can find this in : " + each_page[len(pages_path):] + "\n\n")
+                                for elements in filtered_list:
+                                    TagListFile.write(elements + "\n")
+                                TagListFile.write("\n\n")
+                                TagListFile.write("-" * 20)
+                                TagListFile.write("\n")
+                            
+                        # Third Case : Everything else
                         else:
                             filtered_list = []
                             for elements in list_of_tags:
+                                # Save the original value of the "Tag"
+                                org_elements = elements
+
+                                if "<p:tab" in elements or "<p:commandButton" in elements:
+                                    xss_title = elements.find('title')
+                                    if xss_title == -1:
+                                        break
+                                    else:
+                                        elements = elements[xss_title + len("title") + 2:][:elements[xss_title
+                                                    + len("title") + 2:].find('"')]
+                                        
+                                elif "<p:carousel" in elements:
+
+                                    xss_headerText = elements.find('headerText')
+                                    if xss_headerText == -1:
+                                        elements_header = ""
+                                    else:
+                                        elements_header = elements[xss_headerText + len("headerText") + 2:][:elements[xss_headerText
+                                                            + len("headerText") + 2:].find('"')]
+
+                                    xss_footerText = elements.find('footerText')
+                                    if xss_footerText == -1:
+                                        elements_footer = ""
+                                    else:
+                                        elements_footer = elements[xss_footerText + len("footerText") + 2:][:elements[xss_footerText
+                                                            + len("footerText") + 2:].find('"')]
+
+                                    if elements_footer == "" and elements_header == "":
+                                        break
+                                    else:
+                                        elements = elements_header + " - " + elements_footer   
+                                
+                                elif "<p:dataGrid" in elements or "<p:dataList" in elements or "<p:treeTable" in elements:
+                                    xss_emptyMessage = elements.find('emptyMessage')
+                                    if xss_emptyMessage == -1:
+                                        break
+                                    else:
+                                        elements = elements[xss_emptyMessage + len("emptyMessage") + 2:][:elements[xss_emptyMessage
+                                                    + len("emptyMessage") + 2:].find('"')]
+                                        
+                                elif "<p:pickList" in elements:
+                                    xss_addLabel = elements.find('addLabel')
+                                    if xss_addLabel == -1:
+                                        break
+                                    else:
+                                        elements = elements[xss_addLabel + len("addLabel") + 2:][:elements[xss_addLabel
+                                                    + len("addLabel") + 2:].find('"')]
+
+                                elif "<pslideMenu" in elements:
+                                    xss_backLabel = elements.find('backLabel')
+                                    if xss_backLabel == -1:
+                                        break
+                                    else:
+                                        elements = elements[xss_backLabel + len("backLabel") + 2:][:elements[xss_backLabel
+                                                    + len("backLabel") + 2:].find('"')]
+
+                                elif "<p:inputTextarea" in elements:
+                                    xss_completeMethod = elements.find('completeMethod')
+                                    if xss_completeMethod == -1:
+                                        elements_completeMethod = ""
+                                    else:
+                                        elements_completeMethod = elements[xss_completeMethod + len("completeMethod") + 2:][:elements[xss_completeMethod
+                                                                    + len("completeMethod") + 2:].find('"')]
+
+                                    xss_counterTemplate = elements.find('counterTemplate')
+                                    if xss_counterTemplate == -1:
+                                        elements_counterTemplate = ""
+                                    else:
+                                        elements_counterTemplate = elements[xss_counterTemplate + len("counterTemplate") + 2:][:elements[xss_counterTemplate
+                                                                    + len("counterTemplate") + 2:].find('"')]
+
+                                    if elements_counterTemplate == "" and elements_completeMethod == "":
+                                        break
+                                    else:
+                                        elements = elements_completeMethod + " - " + elements_counterTemplate  
+                                
+                                elif "<p:progressBar" in elements:
+                                    xss_labelTemplate = elements.find('labelTemplate')
+                                    if xss_labelTemplate == -1:
+                                        break
+                                    else:
+                                        elements = elements[xss_labelTemplate + len("labelTemplate") + 2:][:elements[xss_labelTemplate
+                                                    + len("labelTemplate") + 2:].find('"')]
+
                                 # "Bundle" indicates data coming from the server (Not user controlled) 
                                 # so we remove strings that contains it, and "#{" indicates dynamic data 
                                 # (Maybe it's user controlled) so we keep it 
                                 if "bundle" not in elements and "#{" in elements:
+                                    elements = org_elements
                                     filtered_list.append(elements.strip("\n"))
-
-                            TagListFile.write("REGEX : " + str_regex + " \n\n")
-                            TagListFile.write("You can find this in : " + each_page + "\n\n")
+                                
                             if filtered_list != []:
+                                TagListFile.write("REGEX : " + str_regex + " \n\n")
+                                TagListFile.write("You can find this in : " + each_page[len(pages_path):] + "\n\n")
                                 for elements in filtered_list:
                                     TagListFile.write(elements + "\n")
-                                    
+                                TagListFile.write("\n\n")
+                                TagListFile.write("-" * 20)
+                                TagListFile.write("\n")
+
                 except (NameError, ValueError, TypeError, FileExistsError, FileNotFoundError) as e:
                     print("[*] An ERROR Occurred [*]")
                     print(e)
